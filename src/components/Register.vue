f7b18a29a7e876598ba75426ee74263ccc60edc3
mvsr <ravilochan2000@gmail.com>
2020-02-13 06:39:27 +0530

    First

---
+++ src/components/Register.vue
@@ -0,0 +1,31 @@
+// eslint-disable-next-line vue/valid-template-root
+<template>
+<div>
+    <h2> Register </h2>
+    <input type="email" name="email" placeholder="Enter Email" v-model="email"/><br>
+    <input type="password" name="password" placeholder="Enter Password" v-model="password"/><br>
+    <button @click="register">Register</button>
+</div>
+</template>
+<script>
+import AuthService from '@/services/AuthService'
+export default {
+  data () {
+    return {
+      form: {
+        email: '',
+        password: ''}
+    }
+  },
+  methods: {
+    async register () {
+      // eslint-disable-next-line no-unused-vars
+      const response = await AuthService.register({
+        email: this.email,
+        password: this.password
+
+      })
+    }
+  }
+}
+</script>
